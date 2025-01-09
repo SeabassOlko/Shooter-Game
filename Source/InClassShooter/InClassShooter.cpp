aac291c0d1e187ca30815b7a0514055375dbef29
SeabassOlko <63417711+SeabassOlko@users.noreply.github.com>
2025-01-08 18:51:53 -0600

    Pushing First Hello World Test
    Summary
    - Game mode says hello world

---
+++ Source/InClassShooter/InClassShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InClassShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InClassShooter, "InClassShooter" );
